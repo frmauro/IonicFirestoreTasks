1675e6e630791c02b6ed45f0991f3b444d9a2b2c
francisco <frmauro8@hotmail.com>
2019-09-02 20:55:35 -0300

    login page

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+
+
+
+@NgModule({
+  imports: [
+    IonicModule.forRoot()
+  ],
+  exports: [
+    BrowserModule,
+    IonicModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+})
+export class CoreModule { }
