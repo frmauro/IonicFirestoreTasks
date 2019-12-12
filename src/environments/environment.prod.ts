0fb107a77f989598c7ba206829e95c65ae23cc21
francisco <frmauro8@hotmail.com>
2019-12-12 07:24:00 -0300

    Implementation build prod firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDYvqFJHjkllKuSATqqxwu-5FQp99vHuuE',
+    authDomain: 'ionic-firestone-tasks-c819d.firebaseapp.com',
+    databaseURL: 'https://ionic-firestone-tasks-c819d.firebaseio.com',
+    projectId: 'ionic-firestone-tasks-c819d',
+    storageBucket: '',
+    messagingSenderId: '760894082649',
+    appId: '1:760894082649:web:31361a921316c7b069ab48'
+  }
 };
